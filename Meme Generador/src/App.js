8dbd6f426ace5ca8ef3eeed239d722aaf258533e
SantiagoVonk <santiagovonknoblock@gmail.com>
2022-09-28 22:08:20 -0300

    Meneradormeme
    Proyecto practica React

---
+++ Meme Generador/src/App.js
@@ -0,0 +1,73 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import html2canvas from 'html2canvas';
+
+function App() {
+  
+
+  const [linea1, setLinea1] = useState('');
+  const [linea2, setLinea2] = useState('');
+  const [imagen, setImagen] = useState('');
+
+  const onChangeLinea1 = function (evento) {
+    setLinea1(evento.target.value);
+  }
+  const onChangeLinea2 = function (evento) {
+    setLinea2(evento.target.value);
+  }
+  const onChangeImagen = function (evento) {
+    setImagen(evento.target.value);
+  }
+  const onChangeExportar = function (evento) {
+    alert('Exportar');
+    html2canvas(document.querySelector("#meme")).then(canvas => {
+      let img = canvas.toDataURL("image/png");
+      let link = document.createElement('a');
+      link.download = 'meme.png';
+      link.href = img;
+      link.click();
+  });
+  }
+
+
+  return (
+    <div className="App">
+      { // 1- Seleccionar picker de memes 
+        // 2- Input text - Primer linea
+        // 3- Input text - Segunda linea
+        // 4- Boton exportar
+      }
+
+      
+
+
+      <select onChange={onChangeImagen}>
+
+      <option value= "fire">Casa en Llamas</option>
+      <option value= "futurama">Futurama</option>
+      <option value= "history">History Channel</option>
+      <option value= "matrix">Matrix</option>
+      <option value= "philosoraptor">Philosoraptor</option>
+      <option value= "smart">Smart Guy</option>
+      
+      </select> <br/ >
+      
+
+      
+      <input onChange={onChangeLinea1} type="text" placeholder='Linea 1' /> <br/ >
+
+      <input onChange={onChangeLinea2} type="text" placeholder='Linea 2'/> <br/ >
+      
+       <button onClick={onChangeExportar}>Exportar</button>  
+
+       <div className='meme' id='meme'>
+        <span>{linea1}</span> <br/ >
+        <span>{linea2}</span>
+        <img src={'/img/' + imagen + '.jpg'} />
+       </div>
+    </div>
+  );
+}
+
+export default App;
